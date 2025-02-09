0f982e330b27cd1b5ead17eff1001cf073d8f325
keiluck <keiluck365@gmail.com>
2025-02-09 12:41:43 +0900

    img

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Image } from 'antd-mobile'
 
 function page() {
   return (
     <div>
       test page
+    <div>
+      <Image
+            src="/images/top_02.gif"
+            // width={300}
+            fit="cover"
+            style={{ width: '100vw', height: '100vh' }}
+          />
+    </div>
     </div>
   )
 }
