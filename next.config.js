1c55bf351340080484d7eb8826a6c4b4758833af
keiluck <keiluck365@gmail.com>
2025-02-10 14:44:08 +0900

    rds safe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Remove the export output since we need server-side features
+    experimental: {
+        serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+    }
 }
 
 module.exports = nextConfig
