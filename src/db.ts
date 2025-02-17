c6ee098fb61bccd38a895512eb38040ce29c65fb
keiluck <keiluck365@gmail.com>
2025-02-17 18:23:07 +0900

    aws

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient | undefined;
-}
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({
-    log: ['query'],
-  })
-
-// const globalForPrisma = globalThis as unknown as { 
-//     prisma: PrismaClient }
-
+// const globalForPrisma = global as unknown as {
+//     prisma: PrismaClient | undefined;
+// }
 // export const prisma =
-//   globalForPrisma.prisma || new PrismaClient()
+//   globalForPrisma.prisma ?? new PrismaClient({
+//     log: ['query'],
+//   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// // const globalForPrisma = globalThis as unknown as { 
+// //     prisma: PrismaClient }
+
+// // export const prisma =
+// //   globalForPrisma.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
