80a9e446fff02ec53618ada839fc2e4130bad357
keiluck <keiluck365@gmail.com>
2025-02-26 02:27:20 +0900

    error

--- components/home/Main.tsx
+++ components/home/Main.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Main(){
-    return <main>11</main>
+    return <main>
+        <div>
+        <Link href="/myList/myTestList">myList</Link><br/>
+        <Link href="/test">test</Link><br/>
+        <Link href="/jianli">jianli</Link><br/>
+        <Link href="/admin/login">login</Link><br/>
+        </div>
+    </main>
 }
